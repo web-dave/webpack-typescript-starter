5a243b9bda18f44e6d4e5cb44e4dc9140fa57660
web-dave <dave@webdave.de>
2017-02-21 15:01:35 +0100

    use better blocks

--- src/es6/var_let_const.ts
+++ src/es6/var_let_const.ts
@@ -5,11 +5,10 @@ if (true) {
     let     block       no          no         
     const   block       no          no reassigning 
     */
-    let topic: string = 'var';
 
 
 
-    if (topic === 'var') {
+    if (true) {
         // scope
         for (var i = 0; i < 5; i++) {
             setTimeout(() => console.log(i), 1);   // => 5,5,5,5,5
@@ -20,11 +19,9 @@ if (true) {
         // hoisting
         console.log(foo);
         var foo: any = 'foo';
-
-        topic = 'let';
     }
 
-    if (topic === 'let') {
+    if (true) {
         // scope
         for (let i = 0; i < 5; i++) {
             setTimeout(() => console.log(i), 1);   // => 0,1,2,3,4
@@ -32,22 +29,21 @@ if (true) {
 
         console.log(i); // => ReferenceError: i is not defined
         // hoisting
-        console.log(bar);
+        // console.log(bar);
         let bar: any = 'bar';
-
-        topic = 'const';
+        console.log(bar);
     }
 
-    if (topic === 'const') {
+    if (true) {
         const user1 = {
-            name: 'web-dvae',
+            name: 'web-dave',
             twitter: '@webdave_de'
         };
 
         this.user1.name = 'web-dave'; // OK
         this.user1 = { name1: 'foo' }; // error on runtime!!
         let user1 = {
-            name: 'web-dvae',
+            name: 'web-dave',
             twitter: '@webdave_de'
         }; // error on compiling!!
 
