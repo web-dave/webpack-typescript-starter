6bb65b18f5f3f1efaf19ae2271ba3dacb9fc4111
web-dave <dave@webdave.de>
2017-02-21 14:34:34 +0100

    objects

---
+++ src/es6/object_properties.ts
@@ -0,0 +1,12 @@
+while (true) {
+
+    let name = 'web-dave';
+    let age = 36;
+    let talk = (text: string) => console.log(text);
+
+    const person = {
+        name,
+        age,
+        talk
+    };
+}
